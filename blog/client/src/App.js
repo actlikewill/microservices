c8d3cde7c2502cba1892bcc374eb292f4f225f4d
actlikewill <wilsongaturu@gmail.com>
2020-11-29 19:36:01 +0300

    add eventbus and moderation service

--- blog/client/src/App.js
+++ blog/client/src/App.js
@@ -4,12 +4,12 @@ import PostList from './PostList';
 
 export default () => {
   return (
-    <div className="container">
-      <h1>Create Post</h1>
-      <PostCreate />
-      <hr />
-      <h1>Posts</h1>
-      <PostList />
+    <div className="container">      
+        <h1>Create Post</h1>
+        <PostCreate />
+        <hr />
+        <h1>Posts</h1>
+        <PostList />      
     </div>
   );
 };
